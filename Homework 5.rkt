502eab5ac8ce878fb7b2c0b74d2a11d395c35431
Jackson Mades <jacksonmades@gmail.com>
2019-10-09 15:13:43 -0400

    Update Homework 5.rkt
    Exercise 1 done

--- Homework 5.rkt
+++ Homework 5.rkt
@@ -3,6 +3,29 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |Homework 5|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #t)))
 ;;ex 1
 
+;A LoS is one of
+; - empty
+; - (cons String LoS)
+; represents a list of strings
+;Examples:
+(define LOS1 empty)
+(define LOS2 (cons "blue" LOS1))
+(define LOS3 (cons "red" LOS2))
+(define LOS4 (cons "blue" LOS3))
+;Template
+;los-temp: LoS -> ???
+(define (los-temp lt)
+  (cond
+    [(empty? lt)...]
+    [(cons? lt)(...(first lt)...
+                   (los-temp (rest lt))...)]))
+
+;;str-in-list-even?/v1: String LoS -> Boolean
+(define (str-in-list-even?/v1 target LoS)
+  (cond
+    [(empty? LoS) true]
+    [(cons? LoS)(if(string=? target (first lt)) (add1(str-in-list-even?/v1 (rest lt)))
+                   (str-in-list-even?/v1 (rest lt)))]))
 ;;ex 2
 
 ;;ex 3
