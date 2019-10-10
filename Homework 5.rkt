b93eb229984156b451a8fdd2d4397b7b146abed9
Jackson Mades <jacksonmades@gmail.com>
2019-10-10 16:20:40 -0400

    Update Homework 5.rkt
    finished ex 3, started 4 but it's not correct yet

--- Homework 5.rkt
+++ Homework 5.rkt
@@ -61,14 +61,70 @@
     [(cons? LoS) (if (string=? target (first LoS)) (not (str-in-list-even?/v2 target (rest LoS)))
                                                     (str-in-list-even?/v2 target (rest LoS)))]))
 
+(check-expect (flipper false) true)
+(check-expect (flipper true) false)
+
 ;flipper: Boolean->Boolean
 ;takes in boolean and changes it state to the opposite
 (define (flipper flop)
   (not flop))
 
-;;ex 3
+;; List of list of numbers is one of:
+; - empty
+; - (cons LoN LoLoN)
 
-;;ex 4
+; (list (1 2) (3 6 8))
+; (list 1 2 3 6 8)
+; (
+
+;(cons 1 2)
+;(cons 1 (cons 2 empty))
+
+;(list 1 2 3) ==
+;(cons 1 (cons 2 (cons 3 empty)))
+
+;;ex 3
+(define LoN-1 (cons (cons 1 (cons 2 empty)) (cons (cons 3 (cons 6 (cons 8 empty))) empty)))
+(define LoN-2 (cons (cons 2 (cons 4 empty)) (cons (cons 4 (cons 10 (cons 12 empty))) empty)))
+
+;list-of-lists: LoL-> ???
+#;(define (LoL-temp lol)
+  (cond
+    [(empty? lol)...]
+    [(cons? lol)
+     (list-temp(first lol))...
+     (LoL-temp(rest lol))...]))
+
+(check-expect (all-the-nums LoN-1) 20)
+(check-expect (all-the-nums LoN-2) 32)
+;all-the-nums: List of Lists -> Number
+;aggregates all the integers in each list to one number
+(define (all-the-nums lolon)
+  (cond [(empty? lolon) 0]
+        [(cons? lolon)
+         (+ (list-add (first lolon))
+               (all-the-nums (rest lolon)))]))
+
+(check-expect (list-add (cons 6 (cons 8 empty))) 14)
+(check-expect (list-add (cons 5 (cons 3 (cons 7 empty)))) 15)
+;list-add: List->Number
+;gets a list and adds it's insides
+(define(list-add l)
+  (cond [(empty? l) 0]
+        [(cons? l)
+         (+ (first l)
+            (list-add (rest l)))]))
+
+;;ex 4 
+(check-expect (flattens LoN-1) (cons 1 (cons 2 (cons 3 (cons 6 (cons 8 empty))))))
+(check-expect (flattens LoN-2) (cons 2 (cons 4 (cons 4 (cons 10 (cons 12 empty))))))
+;flattens: List of Lists -> List
+;appends an entire list of lists into one list
+(define (flattens lolon)
+  (cond [(empty? lolon) empty]
+        [(cons? lolon)
+         (string-append (number->string(first lolon))
+               (flattens (rest lolon)))]))
 
 ;;ex 5
 
